f86e994dcb39a9f3978671142dc8c17f0df0b77d
Adrian San Segundo <adrisanse.ss@gmail.com>
2025-09-12 19:00:00 +0200

    chore: firebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
